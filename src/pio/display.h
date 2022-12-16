5c1c53d721a4b40df064745f9d399232fdd61dac
Anzheeela <roza.harafytdinova555@gmail.com>
2022-12-16 17:23:03 +0300

    перемещение файлов, добавление папок

---
+++ src/pio/display.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "structs.h"
+#include "funcs.h"
+
+//тут как удобнее всего выводить
---
+++ src/pio/parse.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <vector>
+#include "structs.h"
+#include "funcs.h"
+
+std::vector<vec3> vertices();
+
+std::vector<vec2> texture();
+
+std::vector<normal> normal();
+
+std::vector<indextriangle> triangle();
